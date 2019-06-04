c27e8accdddcdbdd537d1f60db6b9af24c7bbb7e
vince <final_m@163.com>
2019-06-04 17:48:23 +0800

    init commit

---
+++ spring-boot-frist/src/main/java/com/example/springbootfrist/SpringBootFristApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfrist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFristApplication.class, args);
+    }
+
+}
